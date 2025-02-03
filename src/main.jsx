a8f0992e41ce7e9afbed815990ecfe104e4830e5
Lawrence <lawrence.dev25@gmail.com>
2025-02-03 08:19:37 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-    <BrowserRouter basename="/portfolio">
-        <Routes>
-        <Route path="/" element={<App />} />
-        </Routes>
-    </BrowserRouter>
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 )
